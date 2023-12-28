15f362e59246c9f215a68a49a18ad55e530f7117
k-teraoka <k-teraoka@ceres-inc.jp>
2023-12-28 18:53:14 +0900

    Figmaのサジェストだけでcssコーディング

--- app.vue
+++ app.vue
@@ -1,19 +1,25 @@
 <template>
   <header class="header">
-    <img class="header__logo" src="/images/logo.svg" alt="LOGO" />
-    <button class="header__menu">
-      <img class="header__menu__icon" src="/images/icon/menu.svg">
-    </button>
+    <div class="header__contents">
+      <img class="header__logo" src="/images/logo.svg" alt="LOGO" />
+      <button class="header__menu">
+        <img class="header__menu__icon" src="/images/icon/menu.svg">
+      </button>
+    </div>
   </header>
 
-  <main>
+  <main class="page">
     <h1 class="page__title">Page 1</h1>
     
     <section class="section feature">
-      <h2 class="section__title">Feature</h2>
+      <h2 class="section__title">
+        <img class="section__title__icon" src="/images/icon/flag.svg">
+        Feature
+      </h2>
       <ul class="feature__list">
         <li v-for="index in 4" class="feature__list__item">
-          <img :src="`/images/feature/img_${index}.png`" :alt="`feature${index}`">
+          <img class="feature__list__item__img" :src="`/images/feature/img_${index}.png`" :alt="`feature${index}`">
+          <hr />
           <h3 class="feature__list__item__title">Title</h3>
           <p class="feature__list__item__description">I am description. I am description. I am description.</p>
           <small class="feature__list__item__annotation">I am annotation. I am annotation. I am annotation.</small>
@@ -22,16 +28,22 @@
     </section>
     
     <section class="section contact">
-      <h2 class="section__title">Contact</h2>
+      <h2 class="section__title">
+        <img class="section__title__icon" src="/images/icon/question.svg">
+        Contact
+      </h2>
       <div class="formGroup">
-        <label for="name">Your Name</label>
-        <input type="text" v-model="contactForm.name" />
+        <label class="formGroup__label" for="name">Your Name</label>
+        <input class="formGroup__input" type="text" placeholder="Taro Yamada" v-model="contactForm.name" />
       </div>
       <div class="formGroup">
-        <label for="name">Comment</label>
-        <textarea v-model="contactForm.comment" />
+        <label class="formGroup__label" for="name">Comment</label>
+        <textarea class="formGroup__textArea" placeholder="comment" v-model="contactForm.comment" />
       </div>
-      <button @click="submitForm">Submit Form</button>
+      <button class="formButton" @click="submitForm">
+        <div>Submit Form</div>
+        <img class="section__title__icon" src="/images/icon/paper-plane.svg">
+      </button>
     </section>
   </main>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/destyle.css@4.0.0/destyle.css' },
+      ],
+    },
+  },
   css: ["@/public/scss/styles.scss"],
   vite: {
     css: {
